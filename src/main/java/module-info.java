b30e80d94c3fd6d20f52ffadd68ec28169614c44
Yiğitcan Uçan <ucanyiit@gmail.com>
2022-05-06 13:05:02 +0300

    chore: Add bootstrapfx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ceng453.frontend.frontend {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
 
     opens ceng453.frontend.frontend to javafx.fxml;
     exports ceng453.frontend.frontend;
