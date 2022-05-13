0b78c9f3e77908fcd4ebbd3bd1b4d272e4b4ea4b
Yiğitcan Uçan <ucanyiit@gmail.com>
2022-05-13 15:13:48 +0300

    feature: Add register/login functions to forms

--- src/main/java/ceng453/frontend/frontend/controllers/LoginController.java
+++ src/main/java/ceng453/frontend/frontend/controllers/LoginController.java
@@ -2,10 +2,13 @@ package ceng453.frontend.frontend.controllers;
 
 import ceng453.frontend.frontend.utils.StageUtils;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +17,12 @@ import java.util.Objects;
 public class LoginController {
 
     private Stage stage;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Label errorLabel;
 
     public void switchToHome1(ActionEvent event) throws IOException {
         this.stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -27,5 +36,16 @@ public class LoginController {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ceng453/frontend/frontend/home2.fxml")));
         stage = StageUtils.modifyStage(stage, new Scene(root));
         stage.show();
+    }
+
+    public void login(ActionEvent event) throws IOException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        if (username.equals("admin") && password.equals("admin")) {
+            switchToHome2(event);
+        }
+        else {
+            errorLabel.setText("Invalid username or password");
+        }
     }
 }
--- src/main/java/ceng453/frontend/frontend/controllers/RegisterController.java
+++ src/main/java/ceng453/frontend/frontend/controllers/RegisterController.java
@@ -2,10 +2,13 @@ package ceng453.frontend.frontend.controllers;
 
 import ceng453.frontend.frontend.utils.StageUtils;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,11 +17,29 @@ import java.util.Objects;
 public class RegisterController {
 
     private Stage stage;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField passwordReminderField;
+    @FXML
+    private Label errorLabel;
 
     public void switchToHome1(ActionEvent event) throws IOException {
         this.stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ceng453/frontend/frontend/home1.fxml")));
         stage = StageUtils.modifyStage(stage, new Scene(root));
         stage.show();
+    }
+
+    public void register(ActionEvent event) throws IOException {
+        if (usernameField.getText().isEmpty() || emailField.getText().isEmpty() || passwordField.getText().isEmpty() || passwordReminderField.getText().isEmpty()) {
+            errorLabel.setText("Please fill all the fields");
+        } else {
+            switchToHome1(event);
+        }
     }
 }
